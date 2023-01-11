7b5a682b11ce79d7228c5ce099dcebab4a210f7f
Daniel Appiah <52110886+yesiamdaniel@users.noreply.github.com>
2023-01-11 02:20:11 -0800

    Update getModels.sh

--- scripts/getModels.sh
+++ scripts/getModels.sh
@@ -9,6 +9,12 @@ fi
 # Set the URL of the file to download
 url="https://github.com/ayoolaolafenwa/PixelLib/releases/download/0.2.0/pointrend_resnet50.pkl"
 
+# Check if the "models" directory exists in "resources"
+if [ ! -d "../resources/models" ]; then
+  # Create the "models" directory in "resources"
+  mkdir -p "../resources/models"
+fi
+
 # Set the destination directory and file
 destination="../resources/models/pointrend_resnet50.pkl"
 
